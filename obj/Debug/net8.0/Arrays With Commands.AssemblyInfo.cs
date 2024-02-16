2a8eecde715ab5a46d1a11a465634b42f2aedb20
Radoinov <radoinov08@gmail.com>
2024-02-16 13:18:25 +0200

    New project MiddleNumbers

--- obj/Debug/net8.0/Arrays With Commands.AssemblyInfo.cs
+++ obj/Debug/net8.0/Arrays With Commands.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Arrays With Commands")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dca09a44c2025762caada105424fe130d16856aa")]
 [assembly: System.Reflection.AssemblyProductAttribute("Arrays With Commands")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Arrays With Commands")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
